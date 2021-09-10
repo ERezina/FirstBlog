83b320d3b30d793385d49efd436511d44d97340e
katng <katng@list.ru>
2021-09-10 17:14:51 +0700

    1/1 first , 100% arbeiten

---
+++ src/main/java/main/DefaultController.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+@Controller
+public class DefaultController {
+    @RequestMapping("/")
+    private String index(){
+        return "index";
+    }
+}
